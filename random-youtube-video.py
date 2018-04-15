74335bc22b9ba17343ff3af4ade71fb0611c1a30
Orçun Özdemir <o540az@gmail.com>
2018-04-15 14:59:27 +0300

    Create random-youtube-video.py

---
+++ random-youtube-video.py
@@ -0,0 +1,25 @@
+from googleapiclient.discovery import build
+import random
+
+DEVELOPER_KEY = 'YOUR_DEVELOPER_KEY'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+prefix = ['IMG ', 'IMG_', 'IMG-', 'DSC ']
+postfix = [' MOV', '.MOV', ' .MOV']
+
+def youtube_search():
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+  search_response = youtube.search().list(
+    q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),
+    part='snippet',
+    maxResults=5
+  ).execute()
+
+  videos = []
+
+  for search_result in search_response.get('items', []):
+    if search_result['id']['kind'] == 'youtube#video':
+      videos.append('%s' % (search_result['id']['videoId']))
+  return (videos[random.randint(0, 2)])
